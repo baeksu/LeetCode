82c4720fd758179e6a966b60b89e226b5a984cc3
baeksu <37931758+baeksu@users.noreply.github.com>
2021-11-03 01:14:53 +0900

    Update best-time-to-buy-and-sell-stock.java

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,7 +1,13 @@
+/*
+1.이전보다 작은수가 나오면 무조건 그 수를 선택하는게 이득이다. 
+이후에 나오는 큰값과의 차이가 더 커지니까
+
+2.선택한 값과 인덱스를 옮기면서 나오는 값들의 차이를 계속 비교
+*/
+
 class Solution {
     public int maxProfit(int[] prices) {
         int start = 0;
-        int end = -1;
         int ans = 0;
         
         for(int i = 1 ; i< prices.length ; i++){
@@ -9,7 +15,6 @@ class Solution {
                 start = i;
             }else if(prices[i] > prices[start]){
                if(ans < prices[i] - prices[start] ){
-                   end = i;
                    ans = prices[i] - prices[start];
                } 
             }
