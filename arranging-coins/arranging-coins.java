4dad08f40b8871e129521c39d45d1b17bc7e18eb
baeksu <37931758+baeksu@users.noreply.github.com>
2021-11-05 12:56:56 +0900

    Time: 1 ms (100.00%), Space: 36.4 MB (52.22%) - LeetHub

---
+++ arranging-coins/arranging-coins.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long left = 0 ; 
+        long right = n;
+        long mid = 0;
+        
+        while(left <= right){
+            mid = left + (right - left)/2;
+            long sum = (mid*(mid+1)) / 2;
+            
+            if(sum == n) return (int)mid;
+            else if(sum < n) left = mid+1;
+            else right = mid-1;
+            
+        }
+        return (int)right;
+    }
+}
