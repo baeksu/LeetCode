b713f698ac9d581d1a789911eb7a8149c24427d4
baeksu <37931758+baeksu@users.noreply.github.com>
2021-12-11 22:03:53 +0900

    Time: 0 ms (100.00%), Space: 35.7 MB (67.77%) - LeetHub

---
+++ valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start = 1;
+        long end = num;
+        
+        while(start <= end){
+            long mid = start + (end-start)/2;
+            long mul = mid*mid;
+            
+            if(mul == num){
+                return true;
+            }else if(mul < num){
+                start = mid +1;
+            }else if(mul > num){
+                end = mid-1;
+            }
+            
+        }
+        return false;
+    }
+}
