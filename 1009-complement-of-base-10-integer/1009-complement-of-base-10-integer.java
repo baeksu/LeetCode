ae8e3a13fc9b3eb3acb3d319e7651c7782718cbc
baeksu <37931758+baeksu@users.noreply.github.com>
2022-01-05 00:21:40 +0900

    Time: 0 ms (100.00%), Space: 37.4 MB (17.84%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -5,21 +5,16 @@ class Solution {
         if(n == 0)
             return 1;
         
+        int ans = 0;
+        int cnt = 0;
         while(n > 0){
-            if(n % 2 == 0){
-                arrayList.add(0);
-            }else{
-                arrayList.add(1);
+            if( n%2 == 0){
+                ans += Math.pow(2 , cnt);
             }
+            cnt++;
             n = n/2;
         }
-        
-        int ans = 0;
-        for(int i = 0 ; i< arrayList.size() ; i++){
-            if( arrayList.get(i) == 0){
-                ans += Math.pow(2 , i);
-            }
-        }
+
         
         return ans;
     }
