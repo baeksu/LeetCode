5bbb8f8a92f3d57c59e6bb5c5cb1cf47038dba94
baeksu <37931758+baeksu@users.noreply.github.com>
2022-10-10 15:03:15 +0900

    Time: 110 ms (31.41%), Space: 42.2 MB (95.10%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
@@ -1,10 +1,23 @@
-
-
 var removeNthFromEnd = function(head, n) {
-    let fast = head, slow = head
-    for (let i = 0; i < n; i++) fast = fast.next
-    if (!fast) return head.next
-    while (fast.next) fast = fast.next, slow = slow.next
-    slow.next = slow.next.next
-    return head
+    let fast = slow = head;
+    
+    // 미리 fast 를 n 만큼 이동을 시키고 이후에 slow를 출발시키면
+    for(let i = 0 ; i<n ; i++){
+        fast = fast.next;
+    }
+    
+    //만약에 fast 가 맨끝을 넘어가버리면 제일 첫번째 head 만 없애만 된다.
+    if(!fast){
+        return head.next;
+    }
+    
+    
+    while(fast.next){
+        fast = fast.next;
+        slow = slow.next;
+    }
+    
+    slow.next = slow.next.next;
+    return head;
+    
 };
