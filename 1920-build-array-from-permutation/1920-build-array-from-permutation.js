6995a0282a7c4ea143faa5bc561ad97c1d1307e9
baeksu <37931758+baeksu@users.noreply.github.com>
2022-10-25 22:12:39 +0900

    Time: 153 ms (19.01%), Space: 45.8 MB (67.81%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -4,9 +4,12 @@
  */
 var buildArray = function(nums) {
     const ans = [];
-    for(let i=0 ; i<nums.length ; i++ ){
-        ans.push(nums[nums[i]]);
-    }
+    
+    nums.forEach((i) =>{
+        ans.push(nums[i]);
+    })
+    
+    
     return ans;
     
 };
